a0897a37321b1f5b03c243046d10245790d87b8c
altaygileren <altaygileren@gmail.com>
2020-02-18 21:01:58 -0500

    Updates

---
+++ precache-manifest.ff03ebad1096d2adf9b4a690689099d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc1abcd99cdaf3634a7eacc0ffdf511",
+    "url": "/nbagame/index.html"
+  },
+  {
+    "revision": "0901c912835ee0e4401f",
+    "url": "/nbagame/static/css/main.fcfc5bb9.chunk.css"
+  },
+  {
+    "revision": "a701496474b7e380344a",
+    "url": "/nbagame/static/js/2.c4f1752f.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/nbagame/static/js/2.c4f1752f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0901c912835ee0e4401f",
+    "url": "/nbagame/static/js/main.72d4f8c7.chunk.js"
+  },
+  {
+    "revision": "4f4df007087386dac73b",
+    "url": "/nbagame/static/js/runtime-main.db4ddf10.js"
+  }
+]);
